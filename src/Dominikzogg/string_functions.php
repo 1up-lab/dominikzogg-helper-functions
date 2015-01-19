dd94d310168ba71a986e1462c7d0736d69049bad
Dominik Zogg <dominik.zogg@gmail.com>
2015-01-19 23:15:59 +0100

    remove numberCmp within string functions

--- src/Dominikzogg/string_functions.php
+++ src/Dominikzogg/string_functions.php
@@ -92,29 +92,6 @@ function camelCaseToUnderscore($string)
 }
 
 /**
- * @param int|float $a
- * @param int|float $b
- * @return int
- * @throws \InvalidArgumentException
- */
-function numberCmp($a, $b)
-{
-    if (!is_numeric($a)) {
-        throw new \InvalidArgumentException('A is not a number!');
-    }
-
-    if (!is_numeric($b)) {
-        throw new \InvalidArgumentException('B is not a number!');
-    }
-
-    if ($a == $b) {
-        return 0;
-    }
-
-    return $a > $b ? 1 : -1;
-}
-
-/**
  * @param string $input
  * @return string
  */
